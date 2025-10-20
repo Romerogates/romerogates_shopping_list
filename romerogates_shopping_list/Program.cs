e71bb790bbec8eadf0b5630805ad5fa1aab8e29e
Anthony Martin Romero <Anthony.martinR@hotmail.be>
2025-10-20 16:23:40 +0200

    test deployement

--- romerogates_shopping_list/Program.cs
+++ romerogates_shopping_list/Program.cs
@@ -1,15 +1,9 @@
-using System.Net;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
-var cs = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Forcer IPv4
-var host = "db.lnjsaxrjzomapnlqfnno.supabase.co";
-var ipv4 = Dns.GetHostAddresses(host).First(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
-cs = cs.Replace(host, ipv4.ToString());
-
 var app = builder.Build();
+
+string cs = builder.Configuration.GetConnectionString("DefaultConnection");
 
 app.MapGet("/dbtest", async () =>
 {
