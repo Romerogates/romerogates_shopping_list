8c33e5ae745ed860348ed417bf61ae2e74134261
Anthony Martin Romero <Anthony.martinR@hotmail.be>
2025-10-20 16:19:21 +0200

    ipv6 problems

--- romerogates_shopping_list/Program.cs
+++ romerogates_shopping_list/Program.cs
@@ -1,24 +1,24 @@
+using System.Net;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
+var cs = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Lire la connection string depuis appsettings.json
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Forcer IPv4
+var host = "db.lnjsaxrjzomapnlqfnno.supabase.co";
+var ipv4 = Dns.GetHostAddresses(host).First(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+cs = cs.Replace(host, ipv4.ToString());
 
 var app = builder.Build();
 
-// Endpoint pour tester la connexion
 app.MapGet("/dbtest", async () =>
 {
     try
     {
-        await using var conn = new NpgsqlConnection(connectionString);
+        await using var conn = new NpgsqlConnection(cs);
         await conn.OpenAsync();
-
-        // Test simple : récupérer la version de PostgreSQL
         await using var cmd = new NpgsqlCommand("SELECT version();", conn);
         var version = await cmd.ExecuteScalarAsync();
-
         return Results.Ok(new { Status = "Connected", PostgreSQLVersion = version });
     }
     catch (Exception ex)
